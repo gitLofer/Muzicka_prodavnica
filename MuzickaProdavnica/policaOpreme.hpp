8248a8bb1bec4b72997cab044e976bd2ca2826dc
vanjakostic <62511596+vanjakostic@users.noreply.github.com>
2020-05-28 18:49:48 +0200

    Update policaOpreme.hpp

--- MuzickaProdavnica/policaOpreme.hpp
+++ MuzickaProdavnica/policaOpreme.hpp
@@ -1,71 +1,78 @@
 #ifndef POLICAOPREME_HPP_INCLUDED
 #define POLICAOPREME_HPP_INCLUDED
 
+#include "lista.hpp"
+#include "opremaZaInstrumente.hpp"
+#include "mikrofon.hpp"
+#include "kontroler.hpp"
+#include "djOprema.hpp"
+#include "mikser.hpp"
+
 class PolicaOpreme
 {
 private:
-    List<OpremaZaInstrumente> listaOpremeZaInstrumente;
-    List<Mikrofon> listaMikrofona;
-    List<Kontroler> listaKontrolera;
-    List<DJOprema> listaDjOpreme;
-    List<Mikser> listaMiksera;
+    Lista<OpremaZaInstrumente> listaOpremeZaInstrumente;
+    Lista<Mikrofon> listaMikrofona;
+    Lista<Kontroler> listaKontrolera;
+    Lista<DJOprema> listaDjOpreme;
+    Lista<Mikser> listaMiksera;
 public:
-void setListuOpremeIn(List<OpremaZaInstrumente> inst)
+void setListuOpremeIn(Lista<OpremaZaInstrumente> inst)
     {
         listaOpremeZaInstrumente = inst;
     }
-    void setListuMikrofona(List<Mikrofon> mikr)
+    void setListuMikrofona(Lista<Mikrofon> mikr)
     {
         listaMikrofona = mikr;
     }
-    void setListuKontrolera(List<Kontroler> kon)
+    void setListuKontrolera(Lista<Kontroler> kon)
     {
         listaKontrolera = kon;
     }
-    void setListuDJOpreme(List<DJOprema> dj)
+    void setListuDJOpreme(Lista<DJOprema> dj)
     {
         listaDjOpreme = dj;
     }
-    void setListuMiksera(List<Mikser> miks)
+    void setListuMiksera(Lista<Mikser> miks)
     {
         listaMiksera = miks;
     }
 
-    List<OpremaZaInstrumente> getlistaOpremeIns()const{return listaVioloncela;}
-    List<Mikrofon> getlistaMikrofona()const{return listaVioloncela;}
-    List<Kontroler> getlistaKontrolera()const{return listaVioloncela;}
-    List<DJOprema> getlistaDJOpreme()const{return listaVioloncela;}
-    List<Mikser> getlistaMiksera()const{return listaVioloncela;}
+    Lista<OpremaZaInstrumente> getlistaOpremeIns()const{return listaOpremeZaInstrumente;}
+    Lista<Mikrofon> getlistaMikrofona()const{return listaMikrofona;}
+    Lista<Kontroler> getlistaKontrolera()const{return listaKontrolera;}
+    Lista<DJOprema> getlistaDJOpreme()const{return listaDjOpreme;}
+    Lista<Mikser> getlistaMiksera()const{return listaMiksera;}
 
     PolicaOpreme& operator+=(Mikrofon& m)
     {
-        listaMikrofona.add(listaMikrofona.size()+1, m);
+        listaMikrofona.dodajElement(listaMikrofona.velicina()+1, m);
         return *this;
     }
     PolicaOpreme& operator+=(OpremaZaInstrumente& opr)
     {
-        listaOpremeZaInstrumente.add(listaOpremeZaInstrumente.size()+1, opr);
+        listaOpremeZaInstrumente.dodajElement(listaOpremeZaInstrumente.velicina()+1, opr);
         return *this;
     }
     PolicaOpreme& operator+=(Kontroler& k)
     {
-        listaKontrolera.add(listaKontrolera.size()+1, k);
+        listaKontrolera.dodajElement(listaKontrolera.velicina()+1, k);
         return *this;
     }
     PolicaOpreme& operator+=(DJOprema& dj)
     {
-        listaDjOpreme.add(listaDjOpreme.size()+1, dj);
+        listaDjOpreme.dodajElement(listaDjOpreme.velicina()+1, dj);
         return *this;
     }
     PolicaOpreme& operator+=(Mikser& mik)
     {
-        listaMiksera.add(listaMiksera.size()+1, mik);
+        listaMiksera.dodajElement(listaMiksera.velicina()+1, mik);
         return *this;
     }
 
     void ispisOpremeIn()
     {
-        if(listaOpremeZaInstrumente.size() == 0)
+        if(listaOpremeZaInstrumente.velicina() == 0)
         {
             cout<<"-->Nema opreme za instrumente u prodavnici"<<endl;
         }
@@ -74,16 +81,16 @@ void setListuOpremeIn(List<OpremaZaInstrumente> inst)
             OpremaZaInstrumente in;
             cout<<"Oprema za instrumente :"<<endl;
 
-            for (int i = 1; i <= listaOpremeZaInstrumente.size(); i++)
+            for (int i = 1; i <= listaOpremeZaInstrumente.velicina(); i++)
             {
-                listaOpremeZaInstrumente.read(i, in);
-                ///in.ispis(); dodaj kad dodas ispis u opremu
+                listaOpremeZaInstrumente.izlistaj(i, in);
+                in.ispisiOpremu();
             }
         }
     }
     void ispisMikrofonaa()
     {
-        if(listaMikrofona.size() == 0)
+        if(listaMikrofona.velicina() == 0)
         {
             cout<<"-->Nema mikrofona u prodavnici"<<endl;
         }
@@ -92,16 +99,16 @@ void setListuOpremeIn(List<OpremaZaInstrumente> inst)
             Mikrofon mikr;
             cout<<"Mikrofoni :"<<endl;
 
-            for (int i = 1; i <= listaMikrofona.size(); i++)
+            for (int i = 1; i <= listaMikrofona.velicina(); i++)
             {
-                listaMikrofona.read(i, mikr);
-                ///mikr.ispisVioloncela();sejm
+                listaMikrofona.izlistaj(i, mikr);
+                mikr.ispisiMikrofon();
             }
         }
     }
     void ispisKontrolerova()
     {
-        if(listaKontrolera.size() == 0)
+        if(listaKontrolera.velicina() == 0)
         {
             cout<<"-->Nema kontrolera u prodavnici"<<endl;
         }
@@ -110,16 +117,16 @@ void setListuOpremeIn(List<OpremaZaInstrumente> inst)
             Kontroler ko;
             cout<<"Kontroleri :"<<endl;
 
-            for (int i = 1; i <= listaKontrolera.size(); i++)
+            for (int i = 1; i <= listaKontrolera.velicina(); i++)
             {
-                listaKontrolera.read(i, ko);
-                ///ko.ispis();sejm
+                listaKontrolera.izlistaj(i, ko);
+                ko.ispisiKontroler();
             }
         }
     }
     void ispisDjOpreme()
     {
-        if(listaDjOpreme.size() == 0)
+        if(listaDjOpreme.velicina() == 0)
         {
             cout<<"-->Nema DJ opreme u prodavnici"<<endl;
         }
@@ -128,16 +135,16 @@ void setListuOpremeIn(List<OpremaZaInstrumente> inst)
             DJOprema dj;
             cout<<"DJ oprema :"<<endl;
 
-            for (int i = 1; i <= listaDjOpreme.size(); i++)
+            for (int i = 1; i <= listaDjOpreme.velicina(); i++)
             {
-                listaDjOpreme.read(i, dj);
-                dj.ispis();
+                listaDjOpreme.izlistaj(i, dj);
+                dj.ispisiDJOprema();
             }
         }
     }
     void ispisMikserova()
     {
-        if(listaMiksera.size() == 0)
+        if(listaMiksera.velicina() == 0)
         {
             cout<<"-->Nema miksera u prodavnici"<<endl;
         }
@@ -146,14 +153,13 @@ void setListuOpremeIn(List<OpremaZaInstrumente> inst)
             Mikser miks;
             cout<<"Mikseri :"<<endl;
 
-            for (int i = 1; i <= listaMiksera.size(); i++)
+            for (int i = 1; i <= listaMiksera.velicina(); i++)
             {
-                listaMiksera.read(i, miks);
-                ///miks.ispis();
+                listaMiksera.izlistaj(i, miks);
+                miks.ispisMiksera();
             }
         }
     }
 
 };
-
-#endif // POLICAOPREME_HPP_INCLUDED
+#endif
