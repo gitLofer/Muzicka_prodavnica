7101c7dfe15838c5c6f099542e4af4c46d76af18
vanjakostic <62511596+vanjakostic@users.noreply.github.com>
2020-05-28 16:25:37 +0200

    Update gitara.hpp

--- MuzickaProdavnica/gitara.hpp
+++ MuzickaProdavnica/gitara.hpp
@@ -1,32 +1,35 @@
 #ifndef GITARA_HPP_INCLUDED
 #define GITARA_HPP_INCLUDED
 
+#include "zicaniInstrument.hpp"
+enum gitaraVrsta {klasicnaGitara, akusticnaGitara, elektroAkusticnaGitara, elektricnaGitara, basGitara};
+
+
 class Gitara : public ZicaniInstrument
 {
 private:
     gitaraVrsta vrstaGitare;
 public:
+
+    int gitaraID;
+    
     Gitara()
     {
-        naziv = "Gitara";
+        
         cena = 9500.00;
         ocena = 9.9;
         proizvodjac = yamaha;
-        boja = "BELA";
-        timbre = "sharp";
         brojZica = 6;
         duzinaZice = 1.33;
         vrstaZica = nikl;
         vrstaGitare = elektricnaGitara;
     }
-    Gitara(DinString naz, float cc, float oo, proizvodjaci pp, DinString bb, DinString tt, int broj, float duzina, zicaVrsta vrs, gitaraVrsta git)
+    Gitara( float cc, float oo, proizvodjaci pp, int broj, float duzina, zicaVrsta vrs, gitaraVrsta git)
     {
-        naziv = naz;
+        
         cena = cc;
         ocena = oo;
         proizvodjac = pp;
-        boja = bb;
-        timbre = tt;
         brojZica = broj;
         duzinaZice = duzina;
         vrstaZica = vrs;
@@ -34,20 +37,18 @@ public:
     }
     Gitara(const Gitara &g)
     {
-        naziv = g.naziv;
         cena = g.cena;
         ocena = g.ocena;
         proizvodjac = g.proizvodjac;
-        boja = g.boja;
-        timbre = g.timbre;
         brojZica = g.brojZica;
         duzinaZice = g.duzinaZice;
         vrstaZica = g.vrstaZica;
         vrstaGitare = g.vrstaGitare;
     }
 
-    void setVrstaGitare(gitaraVrsta vrs){vrstaGitare = vrs;}
+    void setVrstaGitare(gitaraVrsta vrs){vrstaGitare = vrs; }
 
+    int getGitaraID()const;
     string getVrstaGitare()const
     {
         switch(vrstaGitare)
@@ -73,13 +74,137 @@ public:
         }
     }
 
-    void ispisGitare()
-    {
-        ZicaniInstrument :: ispisZicanogInstrumenta();
+    void ispisGitare(){
+        cout<<"ID: "<<getGitaraID()<<endl;
+        ispisZicanogInstrumenta();
         cout<<"Vrsta gitare: "<<getVrstaGitare()<<endl;
+    }
+    
+    void unosGitare(){
+        int br;
+        cout<<"ID: ";
+        cin>>gitaraID;
+        unosZicanogInstrumenta();
+        cout<<"Vrsta gitare[1-Akusticna/2-Bas/3-Elektricna/4-Elektro akusticna/5-Klasicna]: ";
+        cin>>br;
+        switch(br)
+        {
+        case 1:
+            vrstaGitare = akusticnaGitara;
+            break;
+        case 2:
+            vrstaGitare = basGitara;
+            break;
+        case 3:
+            vrstaGitare = elektricnaGitara;
+            break;
+        case 4:
+            vrstaGitare = elektroAkusticnaGitara;
+            break;
+        case 5:
+            vrstaGitare = klasicnaGitara;
+            break;
+
+        }
     }
 
 };
+
+int Gitara::getGitaraID()const {return gitaraID;}
+
+void dodajUFajlGitara()
+{
+	Gitara g;
+	ofstream outFile;
+	outFile.open("GITARA.dat",ios::binary|ios::app);
+	g.unosGitare();
+	outFile.write(reinterpret_cast<char *> (&g), sizeof(Gitara));
+	outFile.close();
+    	cout<<"\n\nGitara dodata u fajl!";
+	cin.ignore();
+	cin.get();
+}
+void ispisiFajlGitara()
+{
+	Gitara g;
+	ifstream inFile;
+	inFile.open("GITARA.dat",ios::binary);
+	if(!inFile)
+	{
+		cout<<"Greska! Ne moze se otvoriti fajl!";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+	cout<<"\n\n\n\t\tSVE GITARE U FAJLU\n\n";
+	while(inFile.read(reinterpret_cast<char *> (&g), sizeof(Gitara)))
+	{
+	    g.ispisGitare();
+		cout<<"\n\n************************************\n";
+	}
+	inFile.close();
+	cin.ignore();
+	cin.get();
+}
+
+void potraziUFajluGitara(int n)
+{
+	Gitara g;
+	ifstream inFile;
+	inFile.open("GITARA.dat",ios::binary);
+	if(!inFile)
+	{
+		cout<<"Greska! Ne moze se otvoriti fajl!";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+	bool flag=false;
+	while(inFile.read(reinterpret_cast<char *> (&g), sizeof(Gitara)))
+	{
+		if(g.getGitaraID()==n)
+		{
+	  		g.ispisGitare();
+			 flag=true;
+		}
+	}
+	inFile.close();
+	if(flag==false)
+		cout<<"\n\nGitara ne postoji!"<<endl;
+	cin.ignore();
+	cin.get();
+}
+
+void obrisiUFajluGitara(int n)
+{
+	Gitara g;
+	ifstream inFile;
+	inFile.open("GITARA.dat",ios::binary);
+	if(!inFile)
+	{
+		cout<<"Greska! Ne moze se otvoriti fajl!";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+	ofstream outFile;
+	outFile.open("TempGitara.dat",ios::out);
+	inFile.seekg(0,ios::beg);
+	while(inFile.read(reinterpret_cast<char *> (&g), sizeof(Gitara)))
+	{
+		if(g.getGitaraID()!=n)
+		{
+			outFile.write(reinterpret_cast<char *> (&g), sizeof(Gitara));
+		}
+	}
+	outFile.close();
+	inFile.close();
+	remove("GITARA.dat");
+	rename("TempGitara.dat","GITARA.dat");
+	cout<<"\n\n\tGitara obrisana!";
+	cin.ignore();
+	cin.get();
+}
 
 
 #endif // GITARA_HPP_INCLUDED
