27fec9d5bcb26834f01d2f232d52be4b69663f32
teodoravranes <62737677+teodoravranes@users.noreply.github.com>
2020-05-28 10:49:28 +0200

    update slusalice
    neke izmene i dodate funkcije za rad sa fajlovima

--- MuzickaProdavnica/slusalice.hpp
+++ MuzickaProdavnica/slusalice.hpp
@@ -8,54 +8,173 @@ class Slusalice : public Proizvod
 private:
     bool saMikrofonom;
     float duzinaKabla;
-    string frekventniOdziv, sistem;
+    char frekventniOdziv[20], sistem[20];
 public:
+
+    int slusaliceID;
+
     Slusalice()
     {
-    naziv = "Nije unet";
     cena = 5999.99;
     ocena = 8.89;
     proizvodjac = yamaha;
     saMikrofonom = true;
     duzinaKabla = 2.5;
-    frekventniOdziv = "16Hz - 20kHz";
-    sistem = "stereo";
     }
 
-    Slusalice(string NAZIV, float CENA, float OCENA, proizvodjaci PROIZVODJAC, bool MIKROFON, float DUZINA, string FO, string SISTEM)
+    Slusalice( float CENA, float OCENA, proizvodjaci PROIZVODJAC, bool MIKROFON, float DUZINA)
     {
-        naziv = NAZIV;
         cena = CENA;
         ocena = OCENA;
         proizvodjac = PROIZVODJAC;
         saMikrofonom = MIKROFON;
         duzinaKabla = DUZINA;
-        frekventniOdziv = FO;
-        sistem = SISTEM;
     }
 
     Slusalice(const Slusalice &s)
     {
-        naziv = s.naziv;
         cena = s.cena;
         ocena = s.ocena;
         proizvodjac = s.proizvodjac;
         saMikrofonom = s.saMikrofonom;
         duzinaKabla = s.duzinaKabla;
-        frekventniOdziv = s.frekventniOdziv;
-        sistem = s.sistem;
     }
 
+    int getSlusaliceID()const;
     bool getMikrofon()const {return saMikrofonom;}
     float getDuzinaKabla()const {return duzinaKabla;}
-    string getFrekventniOdziv()const {return frekventniOdziv;}
-    string getSistem()const {return sistem;}
+    const char* getFrekventniOdziv()const {return frekventniOdziv;}
+    const char* getSistem()const {return sistem;}
 
     void setMikrofon(bool MIKROFON) {saMikrofonom = MIKROFON;}
     void setDuzinaKabla(float DUZINA) {duzinaKabla = DUZINA;}
-    void setFrekventniOdziv(string FO) {frekventniOdziv = FO;}
-    void setSistem(string SISTEM) {sistem = SISTEM;}
+
+    void ispisiSlusalice()
+    {
+        cout<<"ID: "<<getSlusaliceID()<<endl;
+        ispisProizvoda();
+        cout<<"Ima mikrofon: "<<getMikrofon()<<endl;
+        cout<<"Duzina kabla: "<<getDuzinaKabla()<<"m"<<endl;
+        cout<<"Frekventni odziv: "<<getFrekventniOdziv()<<endl;
+        cout<<"Sistem: "<<getSistem()<<endl;
+    }
+
+    void unesiSlusalice()
+    {
+        cout<<"ID: ";
+        cin>>slusaliceID;
+        fflush(stdin);
+        unosProizvoda();
+        cout<<"Ima mikrofon: ";
+        cin>>saMikrofonom;
+        fflush(stdin);
+        cout<<"Duzina kabla: ";
+        cin>>duzinaKabla;
+        fflush(stdin);
+        cout<<"Frekventni odziv: ";
+        cin>>frekventniOdziv;
+        fflush(stdin);
+        cout<<"Sistem: ";
+        cin>>sistem;
+        fflush(stdin);
+    }
 
 };
+
+int Slusalice::getSlusaliceID()const {return slusaliceID;}
+
+void dodajUFajlSlusalice()
+{
+	Slusalice s;
+	ofstream outFile;
+	outFile.open("SLUSALICE.dat",ios::binary|ios::app);
+	s.unesiSlusalice();
+	outFile.write(reinterpret_cast<char *> (&s), sizeof(Slusalice));
+	outFile.close();
+    	cout<<"\n\nSlusalice dodate u fajl!";
+	cin.ignore();
+	cin.get();
+}
+void ispisiFajlSlusalice()
+{
+	Slusalice s;
+	ifstream inFile;
+	inFile.open("SLUSALICE.dat",ios::binary);
+	if(!inFile)
+	{
+		cout<<"Greska! Ne moze se otvoriti fajl!";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+	cout<<"\n\n\n\t\tSVE SLUSALICE U FAJLU\n\n";
+	while(inFile.read(reinterpret_cast<char *> (&s), sizeof(Slusalice)))
+	{
+	    s.ispisiSlusalice();
+		cout<<"\n\n************************************\n";
+	}
+	inFile.close();
+	cin.ignore();
+	cin.get();
+}
+
+void potraziUFajluSlusalice(int n)
+{
+	Slusalice s;
+	ifstream inFile;
+	inFile.open("SLUSALICE.dat",ios::binary);
+	if(!inFile)
+	{
+		cout<<"Greska! Ne moze se otvoriti fajl!";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+	bool flag=false;
+	while(inFile.read(reinterpret_cast<char *> (&s), sizeof(Slusalice)))
+	{
+		if(s.getSlusaliceID()==n)
+		{
+	  		s.ispisiSlusalice();
+			 flag=true;
+		}
+	}
+	inFile.close();
+	if(flag==false)
+		cout<<"\n\nSlusalice ne postoje!"<<endl;
+	cin.ignore();
+	cin.get();
+}
+
+void obrisiUFajluSlusalice(int n)
+{
+	Slusalice s;
+	ifstream inFile;
+	inFile.open("SLUSALICE.dat",ios::binary);
+	if(!inFile)
+	{
+		cout<<"Greska! Ne moze se otvoriti fajl!";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+	ofstream outFile;
+	outFile.open("TempSlusalice.dat",ios::out);
+	inFile.seekg(0,ios::beg);
+	while(inFile.read(reinterpret_cast<char *> (&s), sizeof(Slusalice)))
+	{
+		if(s.getSlusaliceID()!=n)
+		{
+			outFile.write(reinterpret_cast<char *> (&s), sizeof(Slusalice));
+		}
+	}
+	outFile.close();
+	inFile.close();
+	remove("SLUSALICE.dat");
+	rename("TempSlusalice.dat","SLUSALICE.dat");
+	cout<<"\n\n\tSlusalice obrisane!";
+	cin.ignore();
+	cin.get();
+}
 
 #endif
